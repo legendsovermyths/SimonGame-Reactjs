f0183f31d668df6403cc914e49e864cf7c816172
Mask Wolf <maskwolf2000@gmail.com>
2020-07-30 02:47:08 +0530

    add random generation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import './App.css';
 import Box from "./Box"
 
 
 function App() {
 
-  const colors=["green", "red", "yellow", "blue"]
-  const selectedNumer=Math.floor(Math.random()*4+1)
+  //const colors=["green", "red", "yellow", "blue"]
+  var userSelectedList=[]
+  useEffect(() => {
+    document.addEventListener("keydown", selectedNumer);
+    document.addEventListener("click",printNumber);
+
+    return () => {
+      document.removeEventListener("keydown", selectedNumer);
+      document.removeEventListener("click",printNumber);
+    };
+  }, );
+  const printNumber=(e)=>{
+    if(e.target.className.includes("btn")){
+      userSelectedList.push(e.target.id)
+      console.log(userSelectedList);
+    };
+
+  }
+  const selectedNumer=(e)=>{
+
+    console.log(Math.floor(Math.random()*4));
+  }
   return (
     <div className="App">
       <h1 id="level-title">Press A Key to Start</h1>
         <div className="container">
             <div className="row">
-              <Box number={1} selectedNumer={selectedNumer} id="green" className= "btn green"/>
-              <Box number={2} selectedNumer={selectedNumer} id="red" className= "btn red"/>
+              <Box  id="green" className= "btn green"/>
+              <Box  id="red" className= "btn red"/>
             </div>
             <div className="row">
-              <Box number={3} selectedNumer={selectedNumer} id="yellow" className= 'btn yellow'/>
-              <Box number={4} selectedNumer={selectedNumer} id="blue" className= "btn blue"/>
+              <Box id="yellow" className= 'btn yellow'/>
+              <Box id="blue" className= "btn blue"/>
             </div>
         </div>
     </div>
--- src/Box.js
+++ src/Box.js
@@ -1,19 +1,13 @@
 import React, {useState} from "react"
-
+export var target="0"
 function Box(props){
   const [visible,changeVisible]=useState(true)
   const handleClick=(e)=>{
     changeVisible(false)
     setTimeout(()=>changeVisible(true),100)
   }
-  const startGame=()=>{
-  console.log("hi")
-  if(props.number===props.selectedNumer){
-    handleClick()
-  }
- }
   return(
-  <div onKeyPress={startGame} onClick={handleClick} type="button" id={props.id} className={visible?'fadeIn '+props.className :'fadeOut '+props.className }>
+  <div onClick={handleClick} type="button" id={props.id} className={visible?'fadeIn '+props.className :'fadeOut '+props.className }>
   </div>
 )
 
